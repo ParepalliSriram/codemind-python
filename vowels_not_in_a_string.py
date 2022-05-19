f313a2b73a5deadb3031dafad57d12892bc1a252
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-05-19 06:48:51 +0530

    a=str(input()) if 'a' not in a:     print("a",end=' ') if 'e' not in a:     print("e",end=' ') if 'i' not in a:     print("i",end=' ') if 'o' not in a:     print("o",end=' ') if 'u' not in a:     print("u",end=' ') else:     print("0")

---
+++ vowels_not_in_a_string.py
@@ -0,0 +1,13 @@
+a=str(input())
+if 'a' not in a:
+    print("a",end=' ')
+if 'e' not in a:
+    print("e",end=' ')
+if 'i' not in a:
+    print("i",end=' ')
+if 'o' not in a:
+    print("o",end=' ')
+if 'u' not in a:
+    print("u",end=' ')
+else:
+    print("0")
