5e957619d394457c681490f79d343f410ee70cef
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-05-24 23:13:08 +0530

    a=str(input()) s='' d=0 c=0 for i in a:     if i>='A' and i<='Z':         b=ord(i)         i=chr(b+32)     s+=str(i) p='' l=len(s) for j in s:     c+=1     if j!=' ':         p+=str(j)     if j==' ' or c==l:         if p==p[::-1]:             print(True)             break else:     print(False)

---
+++ Palindrome_or_not.py
@@ -0,0 +1,21 @@
+a=str(input())
+s=''
+d=0
+c=0
+for i in a:
+    if i>='A' and i<='Z':
+        b=ord(i)
+        i=chr(b+32)
+    s+=str(i)
+p=''
+l=len(s)
+for j in s:
+    c+=1
+    if j!=' ':
+        p+=str(j)
+    if j==' ' or c==l:
+        if p==p[::-1]:
+            print(True)
+            break
+else:
+    print(False)
