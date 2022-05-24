41eb4cb42225be2f4ef9398ea6b32b401274310c
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-05-24 15:51:02 +0530

    a=str(input()) c=0 s='' m=0 n=0 for j in a:     m+=1 for i in a:     n+=1     if i!=' ':         c+=1     if i==' ' or n==m:         s+=str(c)         c=0 print(min(s))

---
+++ minimum_length_of_words.py
@@ -0,0 +1,15 @@
+a=str(input())
+c=0
+s=''
+m=0
+n=0
+for j in a:
+    m+=1
+for i in a:
+    n+=1
+    if i!=' ':
+        c+=1
+    if i==' ' or n==m:
+        s+=str(c)
+        c=0
+print(min(s))
