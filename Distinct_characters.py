d437795007800c47302c8b896985cad6e2407a48
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-06-02 06:45:03 +0530

    a=str(input()) a=a.lower() c=0 s='' for i in a:
        for j in a:
            if i==' ':
                break
            elif i==j:
                c+=1
        if c==1:
            s+=str(i)
        c=0
    s=sorted(s)
    for i in s:
        if i=='{' or i=='}':
            continue
        else:
            print(i,end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,12 +1,13 @@
 a=str(input())
 a=a.lower()
-m=set(a)
-a=str(m)
 c=0
 s=''
 for i in a:
+    
     for j in a:
-        if i==j:
+        if i==' ':
+            break
+        elif i==j:
             c+=1
     if c==1:
         s+=str(i)
