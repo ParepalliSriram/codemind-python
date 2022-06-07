6300a75eb847efce2fb117087e1451ff6d53a7f6
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-06-07 14:29:06 +0530

    a=int(input()) b=[a] b=list(map(int,input().split())) c=0 d=0 for i in range(0,a):     if i%2!=0:         if b[i]%2!=0:             c+=1 for i in b:     if i%2!=0:         d+=1 if c==d:     print(True) else:     print(False)

---
+++ Strictly_ODD.py
@@ -0,0 +1,16 @@
+a=int(input())
+b=[a]
+b=list(map(int,input().split()))
+c=0
+d=0
+for i in range(0,a):
+    if i%2!=0:
+        if b[i]%2!=0:
+            c+=1
+for i in b:
+    if i%2!=0:
+        d+=1
+if c==d:
+    print(True)
+else:
+    print(False)
