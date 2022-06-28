c6eb1110fb567e2dbe29e57616d08650074dbf6e
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-06-28 21:26:34 +0530

    a=int(input()) while a>10:     c=0     while a!=0:         b=a%10         a=a//10         c+=b     if c<10:         print(c)         break     else:         a=c

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,23 +1,12 @@
 a=int(input())
-c=0
-p=0
-l=0
-while a!=0:
-    b=a%10
-    a=a//10
-    c+=b
-if c>=10:
-    while c!=0:
-        d=c%10
-        c=c//10
-        p+=d
-    if p>=10:
-        while p!=0:
-            e=p%10
-            p=p//10
-            l+=e
-        print(l)
+while a>10:
+    c=0
+    while a!=0:
+        b=a%10
+        a=a//10
+        c+=b
+    if c<10:
+        print(c)
+        break
     else:
-        print(p)
-else:
-    print(c)
+        a=c
