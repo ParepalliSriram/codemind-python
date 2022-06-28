fe8234a39537e45d237634b57d4ec5619537940f
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-06-28 23:17:18 +0530

    a=int(input()) b=[] c=0 while a:     d=a%10     a=a//10     b.append(d) for i in b:     for j in b:         if i==j:             c+=1     if c>1:         print("Not Unique Number")         break     c=0 else:     print("Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,23 +1,17 @@
 a=int(input())
-m=a
-s=''
-while a!=0:
-    c=a%10
-    a=a//10
-    s+=str(c)
-p=''
-for i in s:
-    if i not in p:
-        p+=str(i)
+b=[]
 c=0
-for i in p:
-    c=c*10+int(i)
-d=0
-while c!=0:
-    e=c%10
-    c=c//10
-    d=d*10+e
-if d==m:
-    print("Unique Number")
+while a:
+    d=a%10
+    a=a//10
+    b.append(d)
+for i in b:
+    for j in b:
+        if i==j:
+            c+=1
+    if c>1:
+        print("Not Unique Number")
+        break
+    c=0
 else:
-    print("Not Unique Number")
+    print("Unique Number")
