26c505695c356b35a50274595ff03fb2117c0e59
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-06-29 07:13:40 +0530

    a=input() a=a.lower() s='' c=0 for i in a:     for j in a:         if i==j:             c+=1     if c==1:         print(i)         break     c=0 else:     print("-1")

--- First_unique_character.py
+++ First_unique_character.py
@@ -1,14 +1,14 @@
-a=str(input())
+a=input()
+a=a.lower()
+s=''
 c=0
-h=0
 for i in a:
     for j in a:
         if i==j:
             c+=1
     if c==1:
-        h=1
         print(i)
         break
     c=0
-if h==0:
+else:
     print("-1")
