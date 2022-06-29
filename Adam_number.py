3d84ef26312615fade616f633264c4ed716d22bb
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-06-29 06:47:32 +0530

    def rev(a):     c=0     while a:         b=a%10         a=a//10         c=c*10+b     return c a=int(input()) b=a*a r_g=rev(a) s_r_g=(r_g)**2 if rev(s_r_g)==b:     print(True) else:     print(False)

--- Adam_number.py
+++ Adam_number.py
@@ -1,17 +1,15 @@
+def rev(a):
+    c=0
+    while a:
+        b=a%10
+        a=a//10
+        c=c*10+b
+    return c
 a=int(input())
 b=a*a
-c=0
-while a!=0:
-    d=a%10
-    a=a//10
-    c=c*10+d
-e=c*c
-f=0
-while e!=0:
-    g=e%10
-    e=e//10
-    f=f*10+g
-if f==b:
-    print('True')
+r_g=rev(a)
+s_r_g=(r_g)**2
+if rev(s_r_g)==b:
+    print(True)
 else:
-    print('False')
+    print(False)
