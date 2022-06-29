530585e33e16ec3280325ae9876746758140de5f
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-06-29 07:04:07 +0530

    def pal(a):     b=a     c=0     while a:         d=a%10         a=a//10         c=c*10+d     if c==b:         return 1     else:         return 0 a=int(input()) if pal(a):     print(True) else:     print(False)

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,16 @@
+def pal(a):
+    b=a
+    c=0
+    while a:
+        d=a%10
+        a=a//10
+        c=c*10+d
+    if c==b:
+        return 1
+    else:
+        return 0
 a=int(input())
-d=a
-c=0
-while a!=0:
-    b=a%10
-    a=a//10
-    c=c*10+b
-if c==d:
-    print('True')
+if pal(a):
+    print(True)
 else:
-    print('False')
+    print(False)
