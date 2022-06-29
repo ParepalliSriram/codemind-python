30cbaae5daa269e7f4555521996f2ffe6c79e01a
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-06-29 13:58:56 +0530

    def rev(a):     c=0     while a:         b=a%10         a=a//10         c+=1     return c a=int(input()) b=[a] b=list(map(int,input().split())) c=[] count=0 for i in b:     d=rev(i)     c.append(d) m=min(c) for i in b:     if rev(i)==m:         count+=1 print(count)

--- Minimum_digit_elements.py
+++ Minimum_digit_elements.py
@@ -1,6 +1,6 @@
-def lenght(a):
+def rev(a):
     c=0
-    while a!=0:
+    while a:
         b=a%10
         a=a//10
         c+=1
@@ -8,15 +8,13 @@ def lenght(a):
 a=int(input())
 b=[a]
 b=list(map(int,input().split()))
-c=min(b)
-m=0
-l=0
-while c!=0:
-    d=c%10
-    c=c//10
-    l+=1
-for i in range(0,a):
-        s=lenght(b[i])
-        if s==l:
-            m+=1
-print(m)
+c=[]
+count=0
+for i in b:
+    d=rev(i)
+    c.append(d)
+m=min(c)
+for i in b:
+    if rev(i)==m:
+        count+=1
+print(count)
