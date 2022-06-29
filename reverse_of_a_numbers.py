1b42783f030981d3fca438e4ae1767da869ec092
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-06-29 06:39:44 +0530

    a=int(input()) b=a if a<0:     a=a*(-1) c=0 while a:    d=a%10    a=a//10    c=c*10+d if b<0:     print(c*(-1)) else:     print(c)

--- reverse_of_a_numbers.py
+++ reverse_of_a_numbers.py
@@ -1,8 +1,13 @@
 a=int(input())
-d=a
+b=a
+if a<0:
+    a=a*(-1)
 c=0
-while a!=0:
-    b=a%10
-    a=a//10
-    c=c*10+b
-print(c)
+while a:
+   d=a%10
+   a=a//10
+   c=c*10+d
+if b<0:
+    print(c*(-1))
+else:
+    print(c)
