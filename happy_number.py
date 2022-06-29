bc1fcbb379c342898244213a638630ffd23fc798
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-06-29 06:44:33 +0530

    a=int(input()) c=0 while a>=10:     while a:         b=a%10         a=a//10         c+=b**2     if c<10:         break     else:         a=c         c=0 if c==1 or c==7:     print(True) else:     print(False)

--- happy_number.py
+++ happy_number.py
@@ -1,14 +1,16 @@
 a=int(input())
 c=0
 while a>=10:
-    c=0
-    while a!=0:
+    while a:
         b=a%10
         a=a//10
-        c+=b*b
-    a=c
-    
-if a==1 or a==7:
-    print('True')
+        c+=b**2
+    if c<10:
+        break
+    else:
+        a=c
+        c=0
+if c==1 or c==7:
+    print(True)
 else:
-    print('False')
+    print(False)
