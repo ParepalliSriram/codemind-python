9da377fb3940f850be335bba83014f6821104254
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-06-30 22:37:23 +0530

    a=int(input()) b=[a] b=list(map(int,input().split())) c=set(b) ans=sum(c) print(ans)

--- unique_sum.py
+++ unique_sum.py
@@ -1,14 +1,6 @@
 a=int(input())
 b=[a]
 b=list(map(int,input().split()))
-c=0
-l=0
-for i in range(0,a):
-    for j in range(i,a):
-        if b[i]==b[j]:
-            c+=1
-    if c==1:
-        l+=b[i]
-    c=0
-print(l)
-        
+c=set(b)
+ans=sum(c)
+print(ans)
