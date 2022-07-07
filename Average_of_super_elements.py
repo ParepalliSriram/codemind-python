c9f9f0d366e5c343c700d67ddc9cdc399a72ab70
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-07 14:39:48 +0530

    a=int(input()) b=[a] b=list(map(int,input().split())) c=[] d=[] co=0 cou=0 for i in b:     if b.count(i)==i:         c.append(i) for i in c:     if i not in d:         d.append(i)         co+=i         cou+=1 if cou>0:     ans=co/cou     print(format(ans,".2f")) else:     print("-1")

--- Average_of_super_elements.py
+++ Average_of_super_elements.py
@@ -1,25 +1,20 @@
 a=int(input())
 b=[a]
 b=list(map(int,input().split()))
-add=0
-m=0
-c=0
-l=0
-s=[]
-for i in range(0,a):
-    for j in range(0,a):
-        if b[i]==b[j]:
-            c+=1
-    if c==b[i]:
-        s.append(b[i])
-        l+=1
-    c=0
-if l>0:
-    p=set(s)
-    for i in p:
-        add+=i
-        m+=1
-    ans=add/m
+c=[]
+d=[]
+co=0
+cou=0
+for i in b:
+    if b.count(i)==i:
+        c.append(i)
+for i in c:
+    if i not in d:
+        d.append(i)
+        co+=i
+        cou+=1
+if cou>0:
+    ans=co/cou
     print(format(ans,".2f"))
 else:
     print("-1")
