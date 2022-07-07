fa68c3384269ab623d8928fa81893c513574b8c6
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-07 14:27:21 +0530

    a=int(input()) b=[a] b=list(map(int,input().split())) c=len(b) d=[] for i in range(0,len(b)):     for j in range(c-1,-1,-1):         d.append(b[i])         d.append(b[j])         break     c=c-1     if c==len(b)//2:         break if a%2!=0:     del d[a-1]     d.append(0)     print(*d)
    else:
        print(*d)

--- First_and_Last_pair.py
+++ First_and_Last_pair.py
@@ -1,25 +1,20 @@
 a=int(input())
 b=[a]
 b=list(map(int,input().split()))
-j=0
-k=len(b)
-s=[]
-c=0
-for i in range(0,a):
-    c+=1
-    s.append(b[j])
-    s.append(b[k-1])
-    j+=1
-    k-=1
-    if c==k:
+c=len(b)
+d=[]
+for i in range(0,len(b)):
+    for j in range(c-1,-1,-1):
+        d.append(b[i])
+        d.append(b[j])
         break
-p=''
-if a%2==0:
-    print(*s)
+    c=c-1
+    if c==len(b)//2:
+        break
+if a%2!=0:
+    del d[a-1]
+    d.append(0)
+    print(*d)
+    
 else:
-    for i in s:
-        if str(i) not in p:
-            p+=str(i)
-    for i in p:
-        print(i,end=' ')
-    print("0",end=' ')
+    print(*d)
