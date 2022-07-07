6e9af15a346897ddb64f31d7183eba3089e48ff9
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-07 15:00:48 +0530

    a=int(input()) b=[a] b=list(map(int,input().split())) c=[] count=0 for i in b:     if b.count(i)==i:         count+=1         c.append(i) if count==0:     print("-1") else:     print(min(c),max(c),end=' ')

--- Maximum_and_Minimum.py
+++ Maximum_and_Minimum.py
@@ -1,19 +1,13 @@
 a=int(input())
 b=[a]
 b=list(map(int,input().split()))
-c=0
-l=0
-m=0
-s=''
-for i in range(0,a):
-    for j in range(0,a):
-        if b[i]==b[j]:
-            c+=1
-    if c==b[i]:
-        l+=1
-        s+=str(b[i])
-    c=0
-if l==0:
+c=[]
+count=0
+for i in b:
+    if b.count(i)==i:
+        count+=1
+        c.append(i)
+if count==0:
     print("-1")
 else:
-    print(min(s),max(s),end=' ')
+    print(min(c),max(c),end=' ')
