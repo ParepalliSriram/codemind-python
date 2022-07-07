7343061a681886593259e0247503ff0104aad6c0
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-07 15:18:41 +0530

    a=int(input()) b=[a] b=list(map(int,input().split())) c=[] for i in b:     if b.count(i)==i:         c.append(i) s=[] cou=0 for i in c:     if i not in s:         s.append(i)         cou+=1 if cou==0:     print("-1") else:     print(*s)

--- Elements_same_as_count.py
+++ Elements_same_as_count.py
@@ -1,24 +1,17 @@
 a=int(input())
 b=[a]
 b=list(map(int,input().split()))
-c=0
-l=0
-p=''
-s=''
-for i in range(0,a):
-    for j in range(0,a):
-        if b[i]==b[j]:
-            c+=1
-    if c==b[i]:
-        l+=1
-        s+=str(b[i])
-    c=0
-if l==0:
+c=[]
+for i in b:
+    if b.count(i)==i:
+        c.append(i)
+s=[]
+cou=0
+for i in c:
+    if i not in s:
+        s.append(i)
+        cou+=1
+if cou==0:
     print("-1")
 else:
-    for d in s:
-        if d not in p:
-            p=p+d
-    for j in p:
-        print(j,end=' ')
-    
+    print(*s)
