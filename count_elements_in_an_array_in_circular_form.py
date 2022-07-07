e28ec703df98f9d384a28f572f8ff31c45534112
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-07 15:46:52 +0530

    a=int(input()) b=[a] b=list(map(int,input().split())) b.append(b[0]) b.append(b[1]) c=0 for i in range(1,a+1):     if b[i-1]%2==0 and b[i+1]%2!=0 or b[i-1]%2!=0 and b[i+1]%2==0:         c+=1 print(c)

--- count_elements_in_an_array_in_circular_form.py
+++ count_elements_in_an_array_in_circular_form.py
@@ -1,12 +1,10 @@
 a=int(input())
 b=[a]
 b=list(map(int,input().split()))
-for i in (0,a):
-    b.append(b[i])
-    b.append(b[i+1])
-    break
+b.append(b[0])
+b.append(b[1])
 c=0
-for i in range(1,len(b)-1):
+for i in range(1,a+1):
     if b[i-1]%2==0 and b[i+1]%2!=0 or b[i-1]%2!=0 and b[i+1]%2==0:
         c+=1
 print(c)
