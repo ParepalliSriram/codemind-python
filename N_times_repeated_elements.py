85d59bef7575453f343469afde2afebae4ee9b3e
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-07 15:28:15 +0530

    a=int(input()) b=[a] b=list(map(int,input().split())) c=int(input()) s=[] co=0 for i in b:     if b.count(i)==c:         if i not in s:             s.append(i)             co+=1 if co==0:     print("-1") else:     print(*s)

--- N_times_repeated_elements.py
+++ N_times_repeated_elements.py
@@ -1,25 +1,15 @@
 a=int(input())
 b=[a]
 b=list(map(int,input().split()))
-n=int(input())
-c=0
-l=0
-p=''
-s=''
-for i in range(0,a):
-    for j in range(0,a):
-        if b[i]==b[j]:
-            c+=1
-    if c==n:
-        l+=1
-        s+=str(b[i])
-    c=0
-if l==0:
+c=int(input())
+s=[]
+co=0
+for i in b:
+    if b.count(i)==c:
+        if i not in s:
+            s.append(i)
+            co+=1
+if co==0:
     print("-1")
 else:
-    for d in s:
-        if d not in p:
-            p=p+d
-    for j in p:
-        print(j,end=' ')
-    
+    print(*s)
