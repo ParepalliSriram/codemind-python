ba0078dff18fdfc804652ba9984d3cc688f86603
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-09 10:28:00 +0530

    a=str(input()) l=len(a) b='' c=1 for i in a:     if i!=' ':         b+=i     if i==' ' or c==l-1:         d=ord(min(b))         k=ord(max(b))         print(abs(d-k),end=' ')         b=''     c+=1

--- absolute_difference_of_small_and_large.py
+++ absolute_difference_of_small_and_large.py
@@ -1,15 +1,13 @@
 a=str(input())
-s=''
 l=len(a)
-c=0
-p1=0
-p2=0
+b=''
+c=1
 for i in a:
-    c+=1
     if i!=' ':
-        s+=str(i)
-    if i==' ' or c==l:
-        p1=ord(min(s))
-        p2=ord(max(s))
-        print(abs(p1-p2),end=' ')
-        s=''
+        b+=i
+    if i==' ' or c==l-1:
+        d=ord(min(b))
+        k=ord(max(b))
+        print(abs(d-k),end=' ')
+        b=''
+    c+=1
