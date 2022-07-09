cfafea2cd58d67629b59214887b3b0a6f8c461b4
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-09 08:38:53 +0530

    a=int(input()) b=[a] b=list(map(int,input().split())) e=[] o=[] for i in b:     if i%2==0:         e.append(i)     else:         o.append(i) if len(e)>len(o):     c=0     for i in range(0,len(o)):         c+=1         print(o[i],e[i],end=' ')     for i in range(c,len(e)):         print(e[i],end=' ') else:     c=0     for i in range(0,len(e)):         c+=1         print(o[i],e[i],end=' ')     for i in range(c,len(o)):         print(o[i],end=' ') if a%2!=0:     print("0")

--- odd_even_pairs.py
+++ odd_even_pairs.py
@@ -1,25 +1,26 @@
 a=int(input())
 b=[a]
 b=list(map(int,input().split()))
-c=0
-s=''
-p=''
-l=''
+e=[]
+o=[]
 for i in b:
     if i%2==0:
-        p+=str(i)
-for i in b:
-    if i%2!=0:
-        s+=str(i)
-for i in s:
-    print(i,end=' ')
-    for j in range(c,len(p)):
-        l+=str(p[j])
-        print(p[j],end=' ')
-        break
-    c+=1
-for i in p:
-    if i not in l:
-        print(i,end=' ')
+        e.append(i)
+    else:
+        o.append(i)
+if len(e)>len(o):
+    c=0
+    for i in range(0,len(o)):
+        c+=1
+        print(o[i],e[i],end=' ')
+    for i in range(c,len(e)):
+        print(e[i],end=' ')
+else:
+    c=0
+    for i in range(0,len(e)):
+        c+=1
+        print(o[i],e[i],end=' ')
+    for i in range(c,len(o)):
+        print(o[i],end=' ')
 if a%2!=0:
-    print("0",end=' ')
+    print("0")
