24329304430f9b38dc43067d1e81afda9516be76
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-09 08:48:33 +0530

    a=int(input()) b=[a] b=list(map(int,input().split())) l=set(b) b=list(l) c=0 for i in b:     if i%2!=0:         if b.count(i)==1:             c+=1 print(c)

--- odd_unique_elements_count.py
+++ odd_unique_elements_count.py
@@ -1,15 +1,11 @@
 a=int(input())
 b=[a]
 b=list(map(int,input().split()))
+l=set(b)
+b=list(l)
 c=0
-l=0
-for i in range(0,a):
-    for j in range(i,a):
-        if b[i]==b[j]:
+for i in b:
+    if i%2!=0:
+        if b.count(i)==1:
             c+=1
-    if c==1:
-        if b[i]%2!=0:
-            l+=1
-    c=0
-print(l)
-        
+print(c)
