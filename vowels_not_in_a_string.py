36c7228e5863bc3ac85c5fed0dbaeefff002c383
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-11 16:50:44 +0530

    a=input() c=0 s='' for i in a:     if i in 'aeiouAEIOU':         s+=i l='aeiou'
    for i in l:
        if i not in s:
            c+=1
            print(i,end=' ')
    if c==0:
        print(c)

--- vowels_not_in_a_string.py
+++ vowels_not_in_a_string.py
@@ -1,13 +1,14 @@
-a=str(input())
-if 'a' not in a:
-    print("a",end=' ')
-if 'e' not in a:
-    print("e",end=' ')
-if 'i' not in a:
-    print("i",end=' ')
-if 'o' not in a:
-    print("o",end=' ')
-if 'u' not in a:
-    print("u",end=' ')
-else:
-    print("0")
+a=input()
+c=0
+s=''
+for i in a:
+    if i in 'aeiouAEIOU':
+        s+=i
+l='aeiou'
+
+for i in l:
+    if i not in s:
+        c+=1
+        print(i,end=' ')
+if c==0:
+    print(c)
