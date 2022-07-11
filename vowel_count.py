7d7d442b0e0d304c1cc8a8bd9c4ef8eb11e06d35
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-11 17:08:08 +0530

    a=input() c=0 for i in a:     if i in 'aeiouAEIOU':         c+=1 print(c)

--- vowel_count.py
+++ vowel_count.py
@@ -1,6 +1,6 @@
-a=str(input())
+a=input()
 c=0
 for i in a:
-    if i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U':
+    if i in 'aeiouAEIOU':
         c+=1
 print(c)
