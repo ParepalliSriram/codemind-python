f2698302b4b71bc232cec0de37b418fae6728e17
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-11 18:29:31 +0530

    a=input() b=input() a=a.lower() b=b.lower() s='' for i in a:     if i==' ':         continue     else:         if i in b:             s+=i for i in b:     if i==' ':         continue     else:         if i in a:             s+=i s=sorted(s) p='' for i in s:     if i not in p:         p+=i print(len(p))

--- common_characters_-II.py
+++ common_characters_-II.py
@@ -1,25 +1,23 @@
-a=str(input())
-b=str(input())
+a=input()
+b=input()
 a=a.lower()
 b=b.lower()
 s=''
-p=''
 for i in a:
-    if i not in s:
-        s+=str(i)
-for j in b:
-    if j not in p:
-        p+=str(j)
-    
-c=0
-m=0
-for f in s:
-    for g in p:
-        if f==' ':
-            continue
-        if f==g:
-            c+=1
-    if c==1:
-        m+=1
-    c=0
-print(m)
+    if i==' ':
+        continue
+    else:
+        if i in b:
+            s+=i
+for i in b:
+    if i==' ':
+        continue
+    else:
+        if i in a:
+            s+=i
+s=sorted(s)
+p=''
+for i in s:
+    if i not in p:
+        p+=i
+print(len(p))
