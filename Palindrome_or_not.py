818446386ba5f2b18e4b7525dbaafdab81307d5e
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-11 15:12:38 +0530

    a=input() a=a.lower() if a==a[::-1]:     print(True) else:     print(False)

--- Palindrome_or_not.py
+++ Palindrome_or_not.py
@@ -1,21 +1,6 @@
-a=str(input())
-s=''
-d=0
-c=0
-for i in a:
-    if i>='A' and i<='Z':
-        b=ord(i)
-        i=chr(b+32)
-    s+=str(i)
-p=''
-l=len(s)
-for j in s:
-    c+=1
-    if j!=' ':
-        p+=str(j)
-    if j==' ' or c==l:
-        if p==p[::-1]:
-            print(True)
-            break
+a=input()
+a=a.lower()
+if a==a[::-1]:
+    print(True)
 else:
     print(False)
