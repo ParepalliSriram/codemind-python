51836611ad3742134a690340fa5a273f4d8d75d4
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-11 16:41:35 +0530

    a=input() s='' for i in a:     if i in 'aeiouAEIOU':         s+=i d='' for i in s:     if i not in d:         d+=i print(*d)

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,16 +1,10 @@
-a=str(input())
-c=0
+a=input()
 s=''
 for i in a:
-    if i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U':
-        c+=1
+    if i in 'aeiouAEIOU':
         s+=i
-if c>0:
-    p=''
-    for m in s:
-        if m not in p:
-            p+=m
-    for n in p:
-        print(n,end=' ')
-else:
-    print("-1")
+d=''
+for i in s:
+    if i not in d:
+        d+=i
+print(*d)
