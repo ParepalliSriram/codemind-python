664802f16af9a8d0c1a5fe6a78d35f5690ed3c21
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-11 15:26:56 +0530

    a=input() c=0 d=0 ans=0 a=a.lower() b=a.split() for i in range(0,len(b)):     for j in b[i]:         if j in 'aeiou':             c=1         break     if c==1:         for j in b[::-1]:             if j not in 'aeiou':                 d=1             break         if d==1:             ans+=1     c=0     d=0 print(ans)

--- count_words.py
+++ count_words.py
@@ -1,28 +1,23 @@
-a=str(input())
-l=len(a)
-s=''
+a=input()
 c=0
+d=0
 ans=0
-v=0
-u=0
-for i in a:
-    c+=1
-    if i!=' ':
-        s+=str(i)
-    if i==' ' or c==l:
-        for j in s:
-            m=j
+a=a.lower()
+b=a.split()
+for i in range(0,len(b)):
+    for j in b[i]:
+        if j in 'aeiou':
+            c=1
+        break
+    if c==1:
+        for j in b[::-1]:
+            if j not in 'aeiou':
+                d=1
             break
-        for j in s[::-1]:
-            n=j
-            break
-        if m=='a' or m=='e' or m=='i' or m=='o' or m=='u' or m=='A' or m=='E' or m=='I' or m=='O' or m=='U':
-            v+=1
-        if n!='a' and n!='e' and n!='i' and n!='o' and n!='u' and n!='A' and n!='E' and n!='I' and n!='O' and n!='U':
-            u+=1
-        if u==1 and v==1:
+        if d==1:
             ans+=1
-        s=''
-        v=0
-        u=0
+    c=0
+    d=0
 print(ans)
+        
+    
