7bb0ec4c9dc56eb2445db24dd112d66a682fe19d
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-11 15:46:13 +0530

    a=input() b=a.split() d=[] c=0 for i in b:     for j in i:         if j in 'aeiouAEIOU':             c+=1     d.append(c)     c=0 print(min(d))

--- minimum_vowel_count_in_a_word.py
+++ minimum_vowel_count_in_a_word.py
@@ -1,16 +1,11 @@
-a=str(input())
-l=len(a)
-
+a=input()
+b=a.split()
+d=[]
 c=0
-s=0
-p=''
-for i in a:
-    c+=1
-    if i!=' ':
-        if i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U':
-            s+=1
-    if i==' ' or c==l:
-        p+=str(s)
-        s=0
-print(min(p))
-    
+for i in b:
+    for j in i:
+        if j in 'aeiouAEIOU':
+            c+=1
+    d.append(c)
+    c=0
+print(min(d))
