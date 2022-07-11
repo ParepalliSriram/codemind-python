fb0321b5b0e3a3252bdcd3e142bd690058e3364a
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-11 17:19:02 +0530

    a=input() b=a.split() for i in b:     print(i[::-1],end=' ')

--- reverse_every_word.py
+++ reverse_every_word.py
@@ -1,14 +1,4 @@
-a=str(input())
-l=0
-k=0
-for i in a:
-    l+=1
-s=''
-c=0
-for i in a:
-    k+=1
-    if i!=' ':
-       s+=str(i)
-    if i==' ' or k==l:
-        print(s[::-1],end=' ')
-        s=''
+a=input()
+b=a.split()
+for i in b:
+    print(i[::-1],end=' ')
