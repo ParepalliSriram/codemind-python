a0ef3cec0424189f8f5186a4dfae8c06d28a6b56
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-11 17:17:41 +0530

    a=input() b=a.split() for i in range(len(b)-1,-1,-1):     print(b[i],end=' ')

--- exchange_the_words.py
+++ exchange_the_words.py
@@ -1,14 +1,4 @@
-a=str(input())
-l=0
-k=0
-for i in a:
-    l+=1
-s=''
-c=0
-for i in a[::-1]:
-    k+=1
-    if i!=' ':
-       s+=str(i)
-    if i==' ' or k==l:
-        print(s[::-1],end=' ')
-        s=''
+a=input()
+b=a.split()
+for i in range(len(b)-1,-1,-1):
+    print(b[i],end=' ')
