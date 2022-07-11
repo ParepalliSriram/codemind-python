17f00cd570f3d8dbd2d0e3900eb8beecbaae21df
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-07-11 17:24:22 +0530

    a=input() b=a.split() for i in b:     print(len(i),end=' ')

--- characters_in_each_word.py
+++ characters_in_each_word.py
@@ -1,14 +1,4 @@
-a=str(input())
-c=0
-s=''
-m=0
-n=0
-for j in a:
-    m+=1
-for i in a:
-    n+=1
-    if i!=' ':
-        c+=1
-    if i==' ' or n==m:
-        print(c,end=' ')
-        c=0
+a=input()
+b=a.split()
+for i in b:
+    print(len(i),end=' ')
