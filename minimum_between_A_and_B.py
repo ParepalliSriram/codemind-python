d51e42e03418f5a68078411800222a48888daa6d
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-09-19 06:36:05 +0530

    a=int(input()) arr=list(map(int,input().split())) ar=[] b,c=map(int,input().split()) d=0 for i in arr:     if(i>=b and i<=c):         d+=1         ar.append(i) if(d==0):     print("-1") else:     print(min(ar))

---
+++ minimum_between_A_and_B.py
@@ -0,0 +1,13 @@
+a=int(input())
+arr=list(map(int,input().split()))
+ar=[]
+b,c=map(int,input().split())
+d=0
+for i in arr:
+    if(i>=b and i<=c):
+        d+=1
+        ar.append(i)
+if(d==0):
+    print("-1")
+else:
+    print(min(ar))
