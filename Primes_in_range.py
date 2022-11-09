34694bb8bae082eb280fe3163fa060abaada13da
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-11-09 06:37:04 +0530

    import math def prime(a):     if a<2:         return False     for i in range(2,int(math.sqrt(a))+1):         if a%i==0:             return False     return True a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if prime(i):         c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,15 +1,15 @@
 import math
 def prime(a):
     if a<2:
-        return 0
+        return False
     for i in range(2,int(math.sqrt(a))+1):
-        if(a%i==0):
-            return 0
-    return 1
+        if a%i==0:
+            return False
+    return True
 a=int(input())
 b=int(input())
-count=0
+c=0
 for i in range(a,b+1):
-    if(prime(i)):
-        count+=1
-print(count)
+    if prime(i):
+        c+=1
+print(c)
