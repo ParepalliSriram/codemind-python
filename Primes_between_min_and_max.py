8b4f1d7f45b1a40a196467a16edff9e8c3d75528
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-11-09 06:44:06 +0530

    import math def prime(a):     if a<2:         return False     for i in range(2,int(math.sqrt(a))+1):         if a%i==0:             return False     return True a=int(input()) l=list(map(int,input().split())) mi=min(l) ma=max(l) fi=l.index(mi) si=l.index(ma) if(fi<si):     li=l[fi:si+1] else:     li=l[si:fi+1] c=0 for i in li:     if prime(i):         c+=1 print(c)

--- Primes_between_min_and_max.py
+++ Primes_between_min_and_max.py
@@ -1,34 +1,23 @@
+import math
 def prime(a):
-    c=0
-    for i in range(1,a+1):
+    if a<2:
+        return False
+    for i in range(2,int(math.sqrt(a))+1):
         if a%i==0:
-            c+=1
-    if c==2:
-        return 1
-    else:
-        return 0
+            return False
+    return True
 a=int(input())
-b=list(map(int,input().split()))
-ma=max(b)
-mi=min(b)
-count=0
-for i in range(0,a):
-    if b[i]==ma:
-        f=i
-        break
-for i in range(0,a):
-    if b[i]==mi:
-        s=i
-        break
-if f>s:
-    t=f
-    h=s
+l=list(map(int,input().split()))
+mi=min(l)
+ma=max(l)
+fi=l.index(mi)
+si=l.index(ma)
+if(fi<si):
+    li=l[fi:si+1]
 else:
-    t=s
-    h=f
-c=[]
-c.extend(b[h:t+1])
-for i in c:
+    li=l[si:fi+1]
+c=0
+for i in li:
     if prime(i):
-        count+=1
-print(count)
+        c+=1
+print(c)
