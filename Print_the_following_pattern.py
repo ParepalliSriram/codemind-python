ecf1ec461d9ff39730e51a6da87130bd78fcf584
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-12-24 23:32:51 +0530

    a=int(input()) b=a for i in range(1,a+1):     for k in range(1,a+1):         if k==i or k==a-i+1:             print(b,end=" ")         else:             print(" ",end="");     print()     b-=1;

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,6 +1,10 @@
 a=int(input())
+b=a
 for i in range(1,a+1):
-    for j in range(i,a+1):
-        print(j,end=' ')
-    
+    for k in range(1,a+1):
+        if k==i or k==a-i+1:
+            print(b,end=" ")
+        else:
+            print(" ",end="");
     print()
+    b-=1;
